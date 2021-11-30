1808453459060add66cb1e54d1d9f2db3410dff3
Brian Bishop <bdbishopdev@gmail.com>
2021-11-30 14:56:10 -0500

    Day 2 (post lunch break)

--- src/app/mocks/hypertheory-learning.mock-server.ts
+++ src/app/mocks/hypertheory-learning.mock-server.ts
@@ -2,6 +2,7 @@ import { subDays } from 'date-fns';
 import { createServer, Model, Response } from 'miragejs';
 import { environment } from 'src/environments/environment';
 import { TopicEntity } from '../reducers/topics.reducer';
+
 const topicFixture: TopicEntity[] = [
   { id: '1', description: 'Angular' },
   { id: '2', description: 'TypeScript' },
@@ -32,11 +33,7 @@ export function mockServer() {
         (schema, request) => {
           let attrs = JSON.parse(request.requestBody);
           if (attrs.description === 'tacos') {
-            return new Response(
-              400,
-              {},
-              { errors: ['We do not study tacos here.'] },
-            );
+            return new Response(400, {}, { errors: ['We do not study tacos here.'] });
           } else {
             return schema.create('topics', attrs).attrs;
           }
@@ -55,9 +52,9 @@ export function mockServer() {
           return {
             personalInfo: {
               employeeId: '93939',
-              firstName: 'Jeff',
-              lastName: 'Gonzalez',
-              email: 'jeff@aol.com',
+              firstName: 'Brian',
+              lastName: 'Bishop',
+              email: 'briguy032@aol.com',
             },
             orders: [
               {
