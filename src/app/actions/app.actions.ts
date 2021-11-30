46421230bdcefa70a25eae7f3ef07759e9543f61
Brian Bishop <bdbishopdev@gmail.com>
2021-11-30 09:35:45 -0500

    Day 1 (shame revisited)

---
+++ src/app/actions/app.actions.ts
@@ -0,0 +1,6 @@
+import { Action, createAction, props } from '@ngrx/store';
+
+export const applicationError = createAction(
+  '[app] application error',
+  props<{ message: string; meta: { feature?: string; originalAction: Action } }>(),
+);
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,6 +1,7 @@
 import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
-import { TopicModel } from '../models';
+import { ApplicationErrorModel, TopicModel } from '../models';
 import { ExistenceCheckSelector } from '../validators/already-exists.validator';
+import * as fromAppErrors from './app-error.reducer';
 import * as fromTempTopics from './temp-topics.reducer';
 import * as fromTopics from './topics.reducer';
 
@@ -18,31 +19,35 @@ export const reducers: ActionReducerMap<AppState> = {
 // we are, for purposes of selectors, going to consider each 'branch' of the state a 'feature'
 const selectTopicsState = createFeatureSelector<fromTopics.TopicState>('topics');
 const selectTempTopicsState = createFeatureSelector<fromTempTopics.TempTopicState>('tempTopics');
+const selectApplicationErrorState = createFeatureSelector<fromAppErrors.AppErrorState>('applicationErrors');
 
-const selectAllTopics = createSelector(selectTopicsState, fromTopics.selectAllTopics);
+const selectAllTopicsEntities = createSelector(selectTopicsState, fromTopics.selectAllTopics);
 
-const selectAllTempTopics = createSelector(selectTempTopicsState, fromTempTopics.selectAllTempTopics);
+const selectAllTempTopicsEntities = createSelector(selectTempTopicsState, fromTempTopics.selectAllTempTopics);
+
+const selectAllTopics = createSelector(selectAllTopicsEntities, (entities) => entities.map(makeModelFromTopic));
+const selectAllTempTopics = createSelector(selectAllTempTopicsEntities, (entities) => entities.map(makeModelFromTopic));
+
+function makeModelFromTopic(topic: fromTopics.TopicEntity) {
+  return {
+    entity: topic,
+    meta: {
+      isTemporary: topic.id.toString().startsWith('T'),
+    },
+  } as TopicModel;
+}
 
 export const selectAllMergedTopics = createSelector(selectAllTopics, selectAllTempTopics, (topics, tempTopics) => {
-  const newTopics = topics.map((t) => {
-    return {
-      entity: t,
-      meta: {
-        isTemporary: false,
-      },
-    } as TopicModel;
-  });
-  const newTempTopics = tempTopics.map((t) => {
-    return {
-      entity: t,
-      meta: {
-        isTemporary: true,
-      },
-    } as TopicModel;
-  });
-  return [...newTopics, ...newTempTopics];
+  return [...topics, ...tempTopics];
 });
 export const selectTopicExists: ExistenceCheckSelector = (props: { value: string }) =>
-  createSelector(selectAllTopics, (topics) =>
+  createSelector(selectAllTopicsEntities, (topics) =>
     topics.some((t) => t.description.toLocaleLowerCase().trim() === props.value.toLocaleLowerCase().trim()),
   );
+
+export const selectErrorModel = createSelector(selectApplicationErrorState, (s) => {
+  return {
+    hasError: s.hasError,
+    message: s.message + ' (' + s.feature + ')',
+  } as ApplicationErrorModel;
+});
